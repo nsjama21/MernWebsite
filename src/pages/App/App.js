ea4284da24b9be3db0b263422716b9d56e8ff865
nsjama15 <nsjama21@gmail.com>
2023-01-12 16:18:40 -0500

    first commit

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -5,19 +5,19 @@ import { getUser } from '../../utilities/users-service';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import NavBar from "../../components/NavBar/NavBar"
-import LandingPage from "../LandingPage/LandingPage"
+// import NavBar from "../../components/NavBar/NavBar"
+// import LandingPage from "../LandingPage/LandingPage"
 
 export default function App() {
   const [user, setUser] = useState(getUser());
   return (
     <main className={styles.App}>
-      <NavBar />
+      {/* <NavBar /> */}
       {user ?
         <>
           <Routes>
             {/* client-side route that renders the component instance if the path matches the url in the address bar */}
-            <Route path="/" element={<LandingPage />} />
+            {/* <Route path="/" element={<LandingPage />} /> */}
             <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
             <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
